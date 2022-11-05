8cb8bd68ba7cd9bed513dde86d5566e05d2e2b6f
Love <Dominic.Love@nationalarchives.gov.uk>
2022-11-05 18:56:54 +0000

    initial commit

---
+++ verityworkaround/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="verityworkaround.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>verity workaround</title>
+</head>
+<body>
+    <form id="WebForm1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Lettercode"></asp:Label>  
+             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br /><br />  
+             <asp:Label ID="Label2" runat="server" Text="Class number"></asp:Label>  
+             <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br /><br />  
+             <asp:Label ID="Label3" runat="server" Text="Minimum Reference"></asp:Label>  
+             <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br /><br />  
+             <asp:Label ID="Label4" runat="server" Text="Maximum Reference"></asp:Label>  
+             <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br /><br /><br /> 
+             <asp:Label ID="Label5" runat="server" Text="EditSetID"></asp:Label>  
+             <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox><br /><br /><br />  
+             <asp:Button ID="Button1" runat="server" Text="Add Piece" OnClick="Button1_Click" /> 
+        </div>
+    </form>
+</body>
+</html>
+
+<!--needs to be aligned DL-->
---
+++ verityworkaround/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿// verity workaround dominic.love@nationalarchives.gov.uk
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+// grey dependencies above may not be needed but kept for caution 
+
+namespace verityworkaround
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DataSet ds = new DataSet();
+        SqlConnection con;
+        //declare parameters 
+        SqlCommand cmd = new SqlCommand();
+        SqlParameter sp1 = new SqlParameter();
+        SqlParameter sp2 = new SqlParameter();
+        SqlParameter sp3 = new SqlParameter();
+        SqlParameter sp4 = new SqlParameter();
+        SqlParameter sp5 = new SqlParameter();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"server=; database=;uid=sa;pwd=");
+            cmd.Parameters.Add("@lettercode", SqlDbType.VarChar).Value = TextBox1.Text;
+            cmd.Parameters.Add("@class_no", SqlDbType.Int).Value = TextBox2.Text;
+            cmd.Parameters.Add("@minPRef", SqlDbType.NVarChar).Value = TextBox3.Text;
+            cmd.Parameters.Add("@maxPRef", SqlDbType.NVarChar).Value = TextBox4.Text;
+            cmd.Parameters.Add("@editSetID", SqlDbType.Int).Value = TextBox5.Text;
+            cmd = new SqlCommand("usp_Edit_InsertPiecesToEditSet", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open(); 
+            cmd.ExecuteNonQuery(); // Procedure or function 'usp_Edit_InsertPiecesToEditSet' expects parameter '@lettercode', which was not supplied.'
+
+            con.Close();
+        }
+    }
+}
